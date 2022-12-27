4be665e428a5be81c8650f815f38f5ac631dc000
Irving <averageirving@gmail.com>
2022-12-27 03:05:29 -0500

    commit

---
+++ activations.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+def ReLu(x):
+    return np.maximum(x, 0)
+
+
+def softmax(x):
+    
+    return np.exp(x)/sum(np.exp(x))
+    
+if __name__ == "__main__":
+
+    x = np.array([1.3, 5.1, 2.2, 0.7, 1.1])
+    print(softmax(x))
+    
---
+++ net.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+from activations import ReLu
+from activations import softmax
+
+def init_params(x, y):
+    k = x.shape[1]
+    m = y.shape[0]
+
+    w = np.random.rand(k, m) - 0.5
+    b = np.random.rand(m) - 0.5
+    return w, b
+
+
+def forward(input, w, b, func):
+    z = np.matmul(input, w) + b
+    output = func(z)
+
+    return output
+
+
+def backward(input, func, ):
+    pass
+
