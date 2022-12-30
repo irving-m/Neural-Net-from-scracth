16e6970656af5bfa8ad09570572973a2341169f2
Irving <averageirving@gmail.com>
2022-12-29 20:26:06 -0500

    commit

--- layer.py
+++ layer.py
@@ -6,49 +6,85 @@ from cost import *
 
 
 class Network():
-    def __init__(self, input, target, layers, cost= None):
-        self.input = input
-        self.target = target
+    def __init__(self, input, target, layers, alpha = 0.1, cost= None):
+        self.input = np.array(input).T
+        self.target = np.array(target).T
         self.layers = layers
+        self.alpha = alpha
         self.cost = cost
 
         self.n_layers = len(layers)
         self.n = len(input)
 
+        self.w = []
+        self.b = []
+
+        for i, layer in enumerate(layers):
+            if i == 0:
+                self.w.append(np.random.rand(layer.neurons, len(self.input)) - 0.5)
+            else:
+                self.w.append(np.random.rand(layer.neurons, layers[i - 1].neurons) - 0.5)
+
+            self.b.append(np.random.rand(layer.neurons, 1) - 0.5)
+
+
         self.a_record = []
         self.z_record = []
-        self.delta_record = []
+
+        self.dw_record = []
+        self.db_record = []
 
 
     def forward_prop(self):
-        a = np.array(self.input).T
+        a = self.input
+        self.a_record.append(a)
 
-        for layer in self.layers:
+        for i, layer in enumerate(self.layers):
 
-            z = np.matmul(layer.w, a) + layer.b.reshape(-1, 1)
+            z = np.matmul(self.w[i], a) + self.b[i].reshape(-1, 1)
             a = layer.func(z)
 
             self.z_record.append(z)
             self.a_record.append(a)
-
-        self.output = a
-
+        
     
     def back_prop(self):
-        delta = np.multiply(self.output - self.target, d_ReLu(self.z_record[-1]))
+        delta = np.multiply(self.a_record[-1] - self.target, d_ReLu(self.z_record[-1]))
 
-        for i, (layer, a, z) in reversed(enumerate(zip(self.layers, self.a_record, self.z_record))):
-            dw = np.matmul(delta, a[i - 1].T)
-            db = np.matmul(delta, np.ones(self.n, 1))
+        for i, layer in reversed(list(enumerate(self.layers))):
+            dw = np.matmul(delta, self.a_record[i].T)
+            db = np.matmul(delta, np.ones((self.n, 1)))
 
-            delta = np.multiply(np.matmul(layer.w.T, delta), layer.prime(a[i - 1]))
+            self.dw_record.append(dw)
+            self.db_record.append(db)
+
+            delta = np.multiply(np.matmul(self.w[i].T, delta), layer.prime(self.z_record[i - 1]))
+
+
+    def gradient_desc(self):
+        w_copy = self.w
+        for i, (w, dw) in enumerate(zip(w_copy, self.dw_record[::-1])):
+            self.w[i] = w - self.alpha*dw
+
+        b_copy = self.b
+        for i, (b, db) in enumerate(zip(b_copy, self.db_record[::-1])):
+            self.b[i] = b - self.alpha*db
+
+
+
+    def fit(self):
+        for i in range(1000):
+            self.forward_prop()
+            self.back_prop()
+            self.gradient_desc()
+        
 
 
 class Layer():
-    def __init__(self, w, b, func, prime = None):
-        self.w = w
-        self.b = b
+    def __init__(self, neurons, func, prime = None):
         self.func = func
+        self.neurons = neurons
+        self.prime = prime
 
 
 x = np.array([[2, -3, 0],
@@ -56,17 +92,16 @@ x = np.array([[2, -3, 0],
                 [3, -1, -1],
                 [1, 4, 8]])
 
-net = Network(x, [Layer(np.array([[0.7, -0.3, -0.1],
-                                [2, 1.5, -0.5]]), 
-                        np.array([1.2, .3]),
-                        ReLu
-                        ), 
-                    Layer(np.array([[1.2, -0.6]]),
-                          np.array([1]),
-                          ReLu
-                        )
-                    ]
-                )
+y = np.array([3, 4, 2, -5])
 
-net.forward_prop()
+
+
+net2 = Network(x, y, [Layer(2, ReLu, d_ReLu), 
+                    Layer(1,ReLu, d_ReLu)])
+
+print(net2.w)
+
+net2.fit()
+
+print(net2.w)
 
