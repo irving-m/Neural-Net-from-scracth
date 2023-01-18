c95e687bf486a514b58f6892b54f35ffe484bbb1
Irving <averageirving@gmail.com>
2023-01-18 14:55:26 -0500

    commit

--- test_mnist.py
+++ test_mnist.py
@@ -21,14 +21,11 @@ x_test = x_test.reshape(-1, 28*28)
 Net = Network(x_train/255, y_train,[
     FCLayer(10, relu, drelu),
     FCLayer(10, softmax)
-], loss_func= cross_entropy, epochs= 10001, learning_rate= 0.01, decay_rate= 0.00, momentum= 0.9
+], loss_func= cross_entropy, epochs= 1001, learning_rate= 0.1, decay_rate= 0.001, momentum= 0.5
 )
 
 Net.fit()
 
-data = pd.DataFrame()
-data["iter"] = range(0, Net.epochs)
-data["loss"] = Net.loss_track
+preds = Net.predict(x_test)
 
-sns.lineplot(data, x= "iter", y= "loss")
-plt.show()
+print(Net.accuracy(preds.T, y_test))
